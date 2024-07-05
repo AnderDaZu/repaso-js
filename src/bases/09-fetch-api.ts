3e3e864756e2c3c06c99eb724aa72c4a7d6c66ba
anderDaZu <anderda-96@hotmail.com>
2024-07-05 11:04:35 -0500

    Interfaces de TypeScript

--- src/bases/09-fetch-api.ts
+++ src/bases/09-fetch-api.ts
@@ -1,9 +1,10 @@
+import type { GIFResponse } from "../interfaces/gif.response";
 
 const apiKey = 'XhX74fXNYYViBS6vllwQBNQ7t19UAeoN';
 
 fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
     .then( resp => resp.json() )
-    .then( (body) => console.log(body) )
+    .then( (body: GIFResponse) => console.log(body.data.images.downsized_medium.url) )
     // .then( ({data}) => {
     //     const {url} = data.images.original;
     //     console.log(url);

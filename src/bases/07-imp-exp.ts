c843110c2b10a018a786e50647c3ab3063a5c70c
anderDaZu <anderda-96@hotmail.com>
2024-07-04 21:22:42 -0500

    Tipado de datos -> interface, type

--- src/bases/07-imp-exp.ts
+++ src/bases/07-imp-exp.ts
@@ -3,7 +3,7 @@
 // todo lo que vaya en {} quiere decir que fue por que se exporto de manera explicita -> import { owners } from 'ruta'
 // si no esta en {} quiere decir que se exporto por defecto -> import herores from 'ruta'
 // import heroes, { owners } from "../data/heroes";
-import myHeroes, { owners } from "../data/heroes"; // se puede asignar nombre cstm a la importación de exportación por defecto
+import myHeroes, { owners, type Owner } from "../data/heroes"; // se puede asignar nombre cstm a la importación de exportación por defecto
 
 const getHeroById = (id: number) => {
     return myHeroes.find( hero => hero.id === id ) ?? {};
@@ -13,12 +13,12 @@ export const getHeroById2 = (id: number) => (
     myHeroes.find( hero => hero.id === id ) ?? {}
 )
 
-export const getHeroesBayOwner = ( owner: string ) => {
+export const getHeroesBayOwner = ( owner: Owner ) => {
     return myHeroes.filter( hero => hero.owner === owner ) ?? [];
 }
 
-console.log( getHeroById(1) );
-console.log( getHeroById2(15) );
-console.log( getHeroesBayOwner('DC') );
+// console.log( getHeroById(1) );
+// console.log( getHeroById2(15) );
+// console.log( getHeroesBayOwner('DC') );
 
-console.log({ owners });
+// console.log({ owners });
